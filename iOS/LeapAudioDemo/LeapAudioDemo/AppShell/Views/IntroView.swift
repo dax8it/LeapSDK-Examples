171d266d3b203d9c454d4f46733c619590cbee36
dax8it <alexcovo@gmail.com>
2026-01-12 19:59:17 -0500

    Add looping video background to IntroView

--- iOS/LeapAudioDemo/LeapAudioDemo/AppShell/Views/IntroView.swift
+++ iOS/LeapAudioDemo/LeapAudioDemo/AppShell/Views/IntroView.swift
@@ -1,13 +1,65 @@
 import SwiftUI
+import AVKit
+
+struct LoopingVideoPlayer: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = PlayerUIView(url: url)
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {}
+    
+    class PlayerUIView: UIView {
+        private var playerLayer = AVPlayerLayer()
+        private var playerLooper: AVPlayerLooper?
+        
+        init(url: URL) {
+            super.init(frame: .zero)
+            
+            let asset = AVURLAsset(url: url)
+            let item = AVPlayerItem(asset: asset)
+            let queuePlayer = AVQueuePlayer(playerItem: item)
+            
+            playerLooper = AVPlayerLooper(player: queuePlayer, templateItem: item)
+            
+            queuePlayer.isMuted = true
+            playerLayer.player = queuePlayer
+            playerLayer.videoGravity = .resizeAspectFill
+            layer.addSublayer(playerLayer)
+            
+            queuePlayer.play()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            playerLayer.frame = bounds
+        }
+    }
+}
 
 struct IntroView: View {
     let artist: Artist?
     let onEnter: () -> Void
     
+    private var videoURL: URL? {
+        Bundle.main.url(forResource: "pari_rotate", withExtension: "mp4")
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Color.black.ignoresSafeArea()
+                if let url = videoURL {
+                    LoopingVideoPlayer(url: url)
+                        .ignoresSafeArea()
+                } else {
+                    Color.black.ignoresSafeArea()
+                }
                 
                 VStack(spacing: 40) {
                     Spacer()
